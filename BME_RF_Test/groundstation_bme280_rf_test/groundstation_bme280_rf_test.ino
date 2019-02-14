b9ae58a267b12cff53e0d4de843986e339ea2efd
Unknown <meg4max1@gmail.com>
2019-02-14 23:46:05 +0100

    changed frequency band from 866 to868

--- BME_RF_Test/groundstation_bme280_rf_test/groundstation_bme280_rf_test.ino
+++ BME_RF_Test/groundstation_bme280_rf_test/groundstation_bme280_rf_test.ino
@@ -9,7 +9,7 @@ void setup() {
  pinMode(LED_BUILTIN,OUTPUT);
  digitalWrite(LED_BUILTIN,LOW);
  LoRa.setPins(SX1278_CS, SX1278_RST, SX1278_IRQ);
- if (!LoRa.begin(866E6)) {
+ if (!LoRa.begin(868E6)) {
   digitalWrite(LED_BUILTIN,HIGH);
  }
  Serial.begin(9600);
