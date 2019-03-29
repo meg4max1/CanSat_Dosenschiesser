bb6eec252cc48993a07abe2bef541c05801b06f4
m a r <meg4max1@gmail.com>
2019-03-30 00:29:11 +0100

    groundstation_recieverworking!
    problems with elevation calculation

--- Groundstation_Reciever/Groundstation_Reciever.ino
+++ Groundstation_Reciever/Groundstation_Reciever.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-//#include <Adafruit_ADXL345_U.h>
+#include <Adafruit_ADXL345_U.h>
 
 #include <SPI.h>
 #include <LoRa.h>
@@ -13,9 +13,9 @@
 #define BME_ADDR 0x76 
 
 Adafruit_BME280 bme;
-//Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-String sendSerial;
+String sendSerial = " ";
 
 void setup() {
  pinMode(LED_BUILTIN,OUTPUT);
@@ -24,6 +24,8 @@ void setup() {
  if (!LoRa.begin(868E6)) {
   digitalWrite(LED_BUILTIN,HIGH);
  }
+ bme.begin(BME_ADDR);
+ accel.begin();
  Serial.begin(9600);
 }
 
@@ -31,26 +33,28 @@ void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     while (LoRa.available()) {
-      sendSerial += (char)LoRa.read();
+      Serial.print((char)LoRa.read());
     }
-    sendSerial += readSensors();
+
     Serial.print(",");
-    Serial.println(LoRa.packetRssi());
-    Serial.print("}");
+    Serial.print(LoRa.packetRssi());
+    Serial.println(readSensors());
   }
+ 
 }
 
 String readSensors(){
   int temperature = bme.readTemperature()*10;
   int humidity = bme.readHumidity()*10;
   int pressure = bme.readPressure()*10;
-  //sensors_event_t event; 
-  //accel.getEvent(&event);
-  //int acceleration = (1000 * sqrt(sq(event.acceleration.x) + sq(event.acceleration.y) + sq(event.acceleration.z)));
-  String assembleString = ",";
-  assembleString = assembleString + temperature + "," + humidity + "," + pressure ;//+ "," + acceleration ;
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  int azimuth = atan(event.acceleration.y/event.acceleration.z)*1000;
+  String assembleString = "," ;
+  assembleString = assembleString + temperature + "," + humidity + "," + pressure + "," + azimuth ;
   return assembleString;
 }
+
 
 void blinkled(int frequency){
   pinMode(LED_BUILTIN,OUTPUT);
